1be0462e0ce1f0dff4d01ce0b08a6e3fcba5de5a
wushen007 <56284787+wushen007@users.noreply.github.com>
2024-04-10 14:37:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -23,7 +23,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '花落', // 您的昵称 例如 tangly1024
   BIO: '一个分享生活的普通人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.wzjpersonal.com/', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '花落, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '编程爱好者/历史爱好者', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'wzj123ahu@126.com', // 邮箱地址 例如mail@tangly1024.com
